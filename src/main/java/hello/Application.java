4560fd8c42bad847f8b41cade101245d941bdec0
subbarayulu-lab <75365966+subbarayulu-lab@users.noreply.github.com>
2020-12-14 12:45:56 +0545

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -15,7 +15,7 @@ public class Application {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World... Welcome To Lotakki CICD. Welcome To Clench Programming..................!";
+		return "Hello Docker World... Welcome To Lotakki CICD. Welcome To Clench Programming..................Included Subbu Project!";
 	}
 
 }
